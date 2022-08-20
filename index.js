f601c8ddf32a4112f881010bba85dcc9f6be901a
Billy Kinyanjui <billy.kinyanjui@student.moringaschool.com>
2022-08-20 15:30:48 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards (names, event) {
+    const messages = []
+    for(let i = 0; i < names.length; i++){
+      const message = `Thank you, ${names[i]}, for the wondeful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"))
+
+function countdown(){
+   var countdown = 10;
+   for(;countdown > 0; countdown--){
+    comsole.log(countdown);
+   }
+   console.log(countdown);
+}
